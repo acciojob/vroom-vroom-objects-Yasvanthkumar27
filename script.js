8bf2fcf7716bf8760212bf236b0804153773b1a1
Yasvanth Kumar <yasvanthkumar48@gmail.com>
2025-11-20 18:12:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 // Complete the js code
-function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+function Car(make, model) {
+  // store properties
+  this.make = make;
+  this.model = model;
+}
+
+// add method to Car prototype
+Car.prototype.getMakeModel = function() {
+  return this.make + ' ' + this.model;
+};
+
+function SportsCar(make, model, topSpeed) {
+  // call Car constructor to initialize make & model on this
+  Car.call(this, make, model);
+  // sports car specific property
+  this.topSpeed = topSpeed;
+}
+
+// inherit from Car.prototype
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// add sports-car specific method
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
-window.SportsCar = SportsCar;
+window.SportsCar = SportsCar;
